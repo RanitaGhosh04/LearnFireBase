e22387cd74496c7038e2922195e304227741f502
Ranita Ghosh <ranitaghosh04@gmail.com>
2024-05-28 23:38:49 +0530

    first commit

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBREduQKybjBemhe0GTxmsneNf-YSogoN0",
+  authDomain: "authentication-db931.firebaseapp.com",
+  projectId: "authentication-db931",
+  storageBucket: "authentication-db931.appspot.com",
+  messagingSenderId: "880325275124",
+  appId: "1:880325275124:web:7cc6b4292e9eda27a2dda1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
